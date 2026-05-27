a93e524bcbdc73a865879deb455a885d34b9ae97
shiyas_dx <shiyasdev62@gmail.com>
2026-05-27 10:04:42 +0530

    Time: 344 ms (63.16%), Space: 15.9 MB (42.11%) - LeetHub

---
+++ 3121-count-the-number-of-special-characters-ii/3121-count-the-number-of-special-characters-ii.py
@@ -0,0 +1,32 @@
+class Solution(object):
+    def numberOfSpecialChars(self, word):
+        """
+        :type word: str
+        :rtype: int
+        """
+        c = 0
+        up = []
+        low = []
+
+        for i in word:
+            if i.isupper():
+                up.append(i)
+            elif i.islower():
+                low.append(i)
+            else:
+                pass
+
+        
+
+        up = set(up)
+        low = set(low)
+
+        
+        for x in low:
+            if x.upper() in up:
+                last_lower = word.rfind(x)
+                first_upper = word.find(x.upper())
+                if last_lower < first_upper:
+                    c += 1
+
+        return c
